7fe9799d5c9859361c9e8b9d9f475aebe77bcda1
Arthur Henrique Vieira Busanello <tuti.busanello@hotmail.com>
2023-10-25 20:39:30 -0300

    completei um pouco aguardando client

--- src/app/components/adress/adressdetails/adressdetails.component.ts
+++ src/app/components/adress/adressdetails/adressdetails.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Adress } from 'src/app/models/adress';
+import { AdressService } from 'src/app/services/adress.service';
 
 @Component({
   selector: 'app-adressdetails',
@@ -6,5 +9,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./adressdetails.component.scss']
 })
 export class AdressdetailsComponent {
+  @Input() adress: Adress = new Adress();
+  @Output() retorno = new EventEmitter<Adress>();
+
+  modalService = inject(NgbModal);
+  modalRef!: NgbModalRef;
+
+  adressService = inject(AdressService);
+
+  constructor() {
+
+  }
+  salvar() {
+    //ISSO AQUI SERVE PARA EDITAR OU ADICIONAR... TANTO FAZ
+
+    this.adressService.save(this.adress).subscribe({
+      next: this.adress => { // QUANDO DÁ CERTO
+        this.retorno.emit(this.adress);
+      },
+      error: erro => { // QUANDO DÁ ERRO
+        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
+        console.error(erro);
+      }
+    });
+
+
+
+  }
+
+
+  excluir(adress: Adress, indice: number) {
+
+    this.adress.client.splice(indice,1);
+    
+  }
+
+  retornoAdressList(adress: Adress) {
+
+    if (this.pedido.produtos == null)
+      this.pedido.produtos = [];
+
+    this.adress.client.push(client);
+    this.modalRef.dismiss();
+}
+
+
+  lancar(modal: any) {
+    this.modalRef = this.modalService.open(modal, { size: 'lg' });
+  }
 
 }
---
+++ src/app/services/adress.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Adress } from '../models/adress';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdressService {
+
+  API: string = 'http://localhost:8080/api/adress';
+  http = inject(HttpClient);
+
+  constructor() { }
+
+
+  listAll(): Observable<Adress[]> {
+    return this.http.get<Adress[]>(this.API);
+  }
+
+  save(adress: Adress): Observable<Adress> {
+    return this.http.post<Adress>(this.API, adress);
+  }
+
+  exemploErro(): Observable<Adress[]> {
+    return this.http.get<Adress[]>(this.API + '/erro');
+  }
+
+
+
+  /*
+  CASO PRECISE ENVIAR REQUEST PARAMS, BASTA DECLARAR ASSIM E INCLUIR NA REQUISIÇÃO HTTP
+
+  let params = new HttpParams()
+      .set('empresaId', empresaId.toString())
+
+  return this.http.get<Pessoa[]>(this.API, { params: params});
+
+  
+  
+  SE PRECISAR COLOCAR COISAS NO HEADER DA REQUISIÇÃO
+
+
+      let headers = new HttpHeaders()
+      .set("Content-Type", "application/json");
+
+
+        return this.http.get<Pessoa[]>(this.API, { headers: headers});
+
+
+
+  */
+
+
+}
